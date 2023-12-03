6bb467baef6f6b5ca80ed3d3b1ecb6c8cfa562cf
Lilypad <hyuntaenyc@gmail.com>
2023-12-03 16:37:47 -0500

    y

--- public/login.js
+++ public/login.js
@@ -48,7 +48,6 @@ function LoginForm(props) {
           props.setStatus("");
           props.setShow(false);
           console.log("JSON:", data);
-          window.location.href = "#/Deposit/";
         } catch (err) {
           props.setStatus(text);
           console.log("err:", text);
