b23050df909957396745f60971dc8a13eeba3541
Lilypad <hyuntaenyc@gmail.com>
2023-12-03 15:57:40 -0500

    change

--- public/alldata.js
+++ public/alldata.js
@@ -1,9 +1,11 @@
+const apiUrl = "http://localhost:3000";
+
 function AllData() {
   const [data, setData] = React.useState([]);
 
   React.useEffect(() => {
     // fetch all accounts from API
-    fetch("/account/all")
+    fetch(`${apiUrl}/account/all`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data[0]);
